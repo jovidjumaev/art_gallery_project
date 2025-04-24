88f41c442dabd9936079b50cd9a41c67c9684ea1
MengsrunNit <117285282+MengsrunNit@users.noreply.github.com>
2025-04-24 13:38:40 -0400

    change alias

--- main.py
+++ main.py
@@ -2,28 +2,25 @@ from fastapi import FastAPI, Request, Depends, Query
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from sqlalchemy import text
 from sqlalchemy.orm import Session
 from datetime import date
+from collections import namedtuple
 
 from models.artist import Artist
 from models.collector import Collector
 from models.artwork import Artwork
 from models.sale import Sale
 from database.database import SessionLocal
-from collections import namedtuple
-from routes.report import router as report_router 
+from routes.report import router as report_router
 
+# Sub-application
+gallery_app = FastAPI()
 
-app = FastAPI()
-
-# Static files
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# Templates
+# Static files and templates
+gallery_app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-# Dependency to get DB session
+# DB Dependency
 def get_db():
     db = SessionLocal()
     try:
@@ -31,20 +28,22 @@ def get_db():
     finally:
         db.close()
 
-# Route to show artist data
-@app.get("/artgalleryproject")
+# Routes under /artgalleryproject
+@gallery_app.get("/", response_class=HTMLResponse)
 def read_artists(request: Request):
-
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.get("artgalleryproject/form")
+@gallery_app.get("/form", response_class=HTMLResponse)
 def artist_form(request: Request): 
     return templates.TemplateResponse("form.html", {"request": request})
 
-
-@app.get("/form/artist-form", response_class=HTMLResponse)
+@gallery_app.get("/form/artist-form", response_class=HTMLResponse)
 async def get_artist_form(request: Request):
-    return templates.TemplateResponse("/form/artist-info-form.html", {"request": request})
+    return templates.TemplateResponse("form/artist-info-form.html", {"request": request})
 
+# Include additional routers
+gallery_app.include_router(report_router)
 
-app.include_router(report_router)
+# Main application
+app = FastAPI()
+app.mount("/artgalleryproject", gallery_app)
